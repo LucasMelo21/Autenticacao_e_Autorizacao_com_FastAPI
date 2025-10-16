0edfb2cc1f595a2aa491452c81249c1383ce9999
Lucas Melo <lucasmelo.tec.saude@gmail.com>
2025-10-16 12:39:10 -0300

    criados os models e seus relacionamentos

---
+++ models/usuario_model.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean
+from sqlalchemy.orm import relationship
+
+from core.configs import settings
+
+class UsuarioModel(settings.DBBaseModel):
+    __tablename__ = 'usuarios'
+    id = Column(integer, primary_key=True)
+    nome = Column(String(100), nullable=True)
+    sobrenome = Column(String(100), nullable=True)
+    email = Column(String(100), nullable=False, unique=True)
+    password = Column(String(100), nullable=False, unique=True)
+    eh_admin = Column(Boolean, default=False)
+    artigos = relationship(
+        "ArtigoModel",
+        cascade="all, delete-orphan",
+        back_populates="criador",
+        uselist=True,
+        lazy="joined")
